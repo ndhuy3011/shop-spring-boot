31c5276217375a328cc96780f3e825b3c094de7b
ndhuy <ndhuy30111@gmail.com>
2025-03-17 23:58:12 +0700

    feat: enhance SecurityConfig to load RSA keys from Vault and refactor initialization; update Docker Compose to remove unused volume

--- auth/src/main/java/com/ndhuy/auth/authentication/infrastructure/SecurityConfig.java
+++ auth/src/main/java/com/ndhuy/auth/authentication/infrastructure/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ndhuy.auth.authentication.infrastructure;
 
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
@@ -29,14 +30,33 @@ public class SecurityConfig {
 
     private final VaultTemplate vaultTemplate;
     private RSAPublicKey publicKeyRca;
+    private RSAPrivateKey privateKeyRca;
+
+    @PostConstruct
+    public void init() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        log.info("loangding Vault Key");
+        VaultResponse response = vaultTemplate.read("secret/data/rca-public");
+        Objects.requireNonNull(response, "Vault Null");
+        var data = response.getData();
+        Objects.requireNonNull(data, "Data Vault NULL");
+        String publicKeyPem = data.get("public_key").toString();
+
+        String pemContent = publicKeyPem.replace("-----BEGIN CERTIFICATE-----", "")
+                .replace("-----END CERTIFICATE-----", "").replaceAll("\\s", "");
+        byte[] decoded = Base64.getDecoder().decode(pemContent);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        this.publicKeyRca = (RSAPublicKey) kf.generatePublic(spec);
+        this.privateKeyRca = (RSAPrivateKey) kf.generatePrivate(spec);
+    }
 
     public SecurityConfig(VaultTemplate vaultTemplate) {
-        this.vaultTemplate = vaultTemplate; // VaultTemplate được inject
+        this.vaultTemplate = vaultTemplate;
     }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        log.info("SecurityConfig");
+        log.info("Loading Security Filter Chain");
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll())
@@ -50,27 +70,14 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @PostConstruct
-    public void init() throws InvalidKeySpecException, NoSuchAlgorithmException {
-        // Chạy sau khi VaultTemplate sẵn sàng
-        VaultResponse response = vaultTemplate.read("secret/data/rca-public");
-        Objects.requireNonNull(response, "Vault Null");
-
-        var data = response.getData();
-
-        Objects.requireNonNull(data, "Data Vault NULL");
-        String publicKeyPem = data.get("public_key").toString();
-        String pemContent = publicKeyPem.replace("-----BEGIN CERTIFICATE-----", "")
-                .replace("-----END CERTIFICATE-----", "").replaceAll("\\s", "");
-        byte[] decoded = Base64.getDecoder().decode(pemContent);
-        X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-        this.publicKeyRca = (RSAPublicKey) kf.generatePublic(spec);
+    @Bean
+    RSAPublicKey publicKeyRca() {
+        return this.publicKeyRca;
     }
 
     @Bean
-    public RSAPublicKey publicKeyRca() {
-        return this.publicKeyRca; // Trả về giá trị đã khởi tạo
+    RSAPrivateKey privateKey() {
+        return this.privateKeyRca;
     }
 
 }
---
+++ vertor/guilder/create-vault.md
@@ -0,0 +1,11 @@
+# vault operator init
+
+int vault: vault operator init
+
+unseal vault: vault operator unseal xxxxxxxxxxxxxxxxxxxx
+
+login vault: vault login hvs.xxxxxxxxxxxx
+
+create method secrets : vault secrets enable -path=authentication-client kv
+
+put key in path : vault kv put authentication-client/private_key
