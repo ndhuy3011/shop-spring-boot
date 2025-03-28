6e6627d39a755059ae1f8915ed9a5f8db4183c13
duchuy <ndhuy30111@gmail.com>
2025-03-28 17:54:15 +0700

    feat: add endpoint to remove role from user and integrate RemoveRoleService

--- auth/src/main/java/com/ndhuy/auth/user/application/rest/RoleAUserRestPrivate.java
+++ auth/src/main/java/com/ndhuy/auth/user/application/rest/RoleAUserRestPrivate.java
@@ -2,15 +2,21 @@ package com.ndhuy.auth.user.application.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ndhuy.auth.user.application.dto.AddRoleDto.AddRoleToAccountIn;
 import com.ndhuy.auth.user.application.dto.AddRoleDto.AddRoleToAccountOut;
+import com.ndhuy.auth.user.application.dto.RemoveRoleDto.RemoveRoleUserIn;
+import com.ndhuy.auth.user.application.dto.RemoveRoleDto.RemoveRoleUserOut;
 import com.ndhuy.auth.user.application.service.AddRoleToAccountService;
+import com.ndhuy.auth.user.application.service.RemoveRoleService;
 
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("api/private/role/user")
@@ -18,11 +24,17 @@ public class RoleAUserRestPrivate {
 
     @Resource
     AddRoleToAccountService addRoleToAccountService;
+    @Resource
+    RemoveRoleService removeRoleService;
 
-    @PostMapping("path")
-    public ResponseEntity<AddRoleToAccountOut> postAddRoleToAccount(@RequestBody AddRoleToAccountIn cpln) {
-
+    @PostMapping("/add")
+    public ResponseEntity<AddRoleToAccountOut> postAddRoleToAccount(@Valid  @RequestBody AddRoleToAccountIn cpln) {
         return ResponseEntity.ok().body(addRoleToAccountService.doMain(cpln));
+    }
+
+    @PutMapping("/remove")
+    public ResponseEntity<RemoveRoleUserOut> putRemoveRoleUser(@Valid @RequestBody RemoveRoleUserIn cpln) {
+        return ResponseEntity.ok().body( removeRoleService.removeRoleAUser(cpln));
     }
 
 }
