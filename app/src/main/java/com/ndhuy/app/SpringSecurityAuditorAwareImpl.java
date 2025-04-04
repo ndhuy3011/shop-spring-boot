c30e1561204143d4915b33f77a394435befc54c2
duchuy <ndhuy30111@gmail.com>
2025-04-04 15:10:38 +0700

    refactor: replace ABaseEntity with EntityBase for improved auditing support

--- app/src/main/java/com/ndhuy/app/SpringSecurityAuditorAwareImpl.java
+++ app/src/main/java/com/ndhuy/app/SpringSecurityAuditorAwareImpl.java
@@ -2,22 +2,32 @@ package com.ndhuy.app;
 
 import java.util.Optional;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
-public class SpringSecurityAuditorAwareImpl implements AuditorAware<User>{
+@Configuration
+@EnableJpaAuditing
+public class SpringSecurityAuditorAwareImpl implements AuditorAware<User> {
 
-    @Override
-    public Optional<User> getCurrentAuditor() {
-  
-      return Optional.ofNullable(SecurityContextHolder.getContext())
-              .map(SecurityContext::getAuthentication)
-              .filter(Authentication::isAuthenticated)
-              .map(Authentication::getPrincipal)
-              .map(User.class::cast);
-    }
-    
+  @Override
+  public Optional<User> getCurrentAuditor() {
+
+    return Optional.ofNullable(SecurityContextHolder.getContext())
+        .map(SecurityContext::getAuthentication)
+        .filter(Authentication::isAuthenticated)
+        .map(Authentication::getPrincipal)
+        .map(User.class::cast);
+  }
+
+  @Bean
+  AuditorAware<User> auditorProvider() {
+    return new SpringSecurityAuditorAwareImpl();
+  }
+
 }
