4189e80b31f9c8cbed6ee86e6bf020019f3d6d1a
Nguyễn Đức Huy <ndhuy30111@gmail.com>
2025-04-04 15:11:18 +0700

    refactor: replace ABaseEntity with EntityBase for improved auditing support (#13)

--- app/src/main/java/com/ndhuy/app/EntityBase.java
+++ app/src/main/java/com/ndhuy/app/EntityBase.java
@@ -6,30 +6,47 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public abstract class ABaseEntity {
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class EntityBase {
+    @Getter
+    @Setter
     @CreatedDate
     private Instant createdDate;
+    @Getter
+    @Setter
     @LastModifiedDate
     private Instant lastModifiedDate;
+    @Getter
+    @Setter
     @CreatedBy
     private String createdBy;
+    @Getter
+    @Setter
     @LastModifiedBy
     private String lastModifiedBy;
 
-    private String deletedBy;
-
-    private Instant deletedDate;
-
     private boolean deleted = false;
-
+    @Getter
+    @Setter
     @Version
     private Long version;
+
+
+    public void delete(){
+        this.deleted = true;
+    }
+    public boolean isDeleted() {
+        return deleted;
+    }
 
 }
